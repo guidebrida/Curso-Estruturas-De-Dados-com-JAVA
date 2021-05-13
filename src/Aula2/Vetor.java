fb80df82fde704c0a31c9f2b2961b28e08b323d3
guidebrida <guilhermedbdb@gmail.com>
2021-05-13 11:19:16 -0300

    first commit

---
+++ src/Aula2/Vetor.java
@@ -0,0 +1,14 @@
+package Aula2;
+
+public class Vetor {
+
+
+    private String[] elementos;
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+    public void adiciona(String elemento){
+
+    }
+}
