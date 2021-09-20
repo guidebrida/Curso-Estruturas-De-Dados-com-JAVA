7fa890903183a50f6a6670dac4e406a6259714e2
guilherme de brida de bona <guilherme.bona@betha.com.br>
2021-09-20 11:02:31 -0300

    filas parte 4

--- src/Aulas/fila/Aula22.java
+++ src/Aulas/fila/Aula22.java
@@ -1,20 +1,24 @@
 package Aulas.fila;
 
-public class Aula21 {
+public class Aula22 {
     public static void main(String[] args) {
 
         Fila<Integer> fila = new Fila<>();
-
 
         fila.enfileira(1);
         fila.enfileira(2);
         fila.enfileira(3);
 
+        System.out.println(fila);
 
-        System.out.println(fila.espiar());
+        System.out.println(fila.desenfileira());
 
         System.out.println(fila);
 
+        System.out.println(fila.desenfileira());
 
+        System.out.println(fila);
     }
+
+
 }
