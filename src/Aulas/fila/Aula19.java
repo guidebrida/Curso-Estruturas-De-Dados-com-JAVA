d86794dcd67f8144ee61ea34a7024e64734381ff
guilherme de brida de bona <guilherme.bona@betha.com.br>
2021-09-20 08:44:17 -0300

    filas parte 1

---
+++ src/Aulas/fila/Aula19.java
@@ -0,0 +1,17 @@
+package Aulas.fila;
+
+public class Aula19 {
+
+    public static void main(String[] args) {
+
+
+        Fila<Integer> fila = new Fila<>();
+
+        System.out.println(fila.estaVazia());
+        System.out.println(fila.tamanho());
+
+    }
+
+
+
+}
---
+++ src/Aulas/fila/Fila.java
@@ -0,0 +1,18 @@
+package Aulas.fila;
+
+import Aulas.pilhas.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica {
+
+
+    public Fila(){
+        super();
+    }
+
+    public Fila(int capacidade){
+        super(capacidade);
+    }
+
+
+
+}
