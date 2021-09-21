7ab4a28e4cdc889a5de023ea4704d7ab99f55ec9
guilherme de brida de bona <guilherme.bona@betha.com.br>
2021-09-21 10:47:54 -0300

    filas parte 6

---
+++ src/Aulas/fila/Aula25.java
@@ -0,0 +1,25 @@
+package Aulas.fila;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Aula25 {
+
+    public static void main(String[] args) {
+        Queue<Paciente> filaComPrioridade = new PriorityQueue<>(
+                new Comparator<Paciente>() {
+                    @Override
+                    public int compare(Paciente p1, Paciente p2) {
+                        return Integer.valueOf(p1.getPrioridade()).compareTo(p2.getPrioridade());
+                    }
+                }
+        );
+
+        filaComPrioridade.add(new Paciente("Maria", 2));
+        filaComPrioridade.add(new Paciente("Guilherme", 1));
+
+        System.out.println(filaComPrioridade);
+
+    }
+}
